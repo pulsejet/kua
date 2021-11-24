e934a44cf87253eb32e6d38e081b7847e917bcbb
Varun Patil <varunpatil@ucla.edu>
2021-11-24 00:49:37 +0000

    Add worker prefix registrations

--- src/worker.cpp
+++ src/worker.cpp
@@ -1,6 +1,7 @@
 #include "worker.hpp"
 
 #include <ndn-cxx/util/logger.hpp>
+#include <thread>
 
 namespace kua {
 
@@ -10,11 +11,66 @@ Worker::Worker(ConfigBundle& configBundle, const Bucket& bucket)
   : m_configBundle(configBundle)
   , m_bucket(bucket)
   , m_nodePrefix(configBundle.nodePrefix)
-  , m_face(configBundle.face)
   , m_scheduler(m_face.getIoService())
   , m_keyChain(configBundle.keyChain)
+  , m_bucketPrefix(ndn::Name(configBundle.kuaPrefix).appendNumber(bucket.id))
 {
-  NDN_LOG_INFO("Constructing worker for #" << bucket.id);
+  NDN_LOG_INFO("Constructing worker for #" << bucket.id << " " << m_nodePrefix);
+
+  // Get all interests
+  m_face.setInterestFilter("/", std::bind(&Worker::onInterest, this, _1, _2));
+
+  // Register for unique node
+  m_face.registerPrefix(ndn::Name(m_nodePrefix).appendNumber(bucket.id),
+                        nullptr, // RegisterPrefixSuccessCallback is optional
+                        std::bind(&Worker::onRegisterFailed, this, _1, _2));
+
+  // Register for bucket
+  m_face.registerPrefix(m_bucketPrefix,
+                        nullptr, // RegisterPrefixSuccessCallback is optional
+                        std::bind(&Worker::onRegisterFailed, this, _1, _2));
+
+  std::thread thread(std::bind(&Worker::run, this));
+  thread.detach();
+}
+
+Worker::~Worker() {
+  m_face.shutdown();
+}
+
+void
+Worker::run()
+{
+  m_face.processEvents();
+}
+
+void
+Worker::onRegisterFailed(const ndn::Name& prefix, const std::string& reason)
+{
+  NDN_LOG_ERROR("ERROR: Failed to register prefix '" << prefix
+             << "' with the local forwarder (" << reason << ")");
+
+  if (m_failedRegistrations >= 5) {
+    m_face.shutdown();
+    return;
+  }
+
+  m_scheduler.schedule(ndn::time::milliseconds(30), [this, prefix] {
+    m_face.registerPrefix(prefix,
+                          nullptr, // RegisterPrefixSuccessCallback is optional
+                          std::bind(&Worker::onRegisterFailed, this, _1, _2));
+  });
+
+  m_failedRegistrations += 1;
+}
+
+void
+Worker::onInterest(const ndn::InterestFilter&, const ndn::Interest& interest)
+{
+  // Ignore interests from localhost
+  if (ndn::Name("localhost").isPrefixOf(interest.getName())) return;
+
+  NDN_LOG_INFO("Interest " << interest);
 }
 
 } // namespace kua
--- src/worker.hpp
+++ src/worker.hpp
@@ -12,14 +12,30 @@ class Worker
 public:
   Worker(ConfigBundle& configBundle, const Bucket& bucket);
 
+  ~Worker();
+
+private:
+  void
+  onRegisterFailed(const ndn::Name& prefix, const std::string& reason);
+
+  void
+  onInterest(const ndn::InterestFilter&, const ndn::Interest& interest);
+
+  void
+  run();
+
 private:
   ConfigBundle& m_configBundle;
   const Bucket& m_bucket;
 
   ndn::Name m_nodePrefix;
-  ndn::Face& m_face;
+  ndn::Face m_face;
   ndn::Scheduler m_scheduler;
   ndn::KeyChain& m_keyChain;
+
+  ndn::Name m_bucketPrefix;
+
+  size_t m_failedRegistrations = 0;
 };
 
 } // namespace kua
